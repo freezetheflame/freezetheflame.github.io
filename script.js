36841216079f84198135aa00e511451e8764a439
Scotchingice <1439921564@qq.com>
2023-04-18 13:01:46 +0800

    update the trial page

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ class Switch {
 		this.removeAttribute("data-pristine");
 	}
 }
+window.addEventListener("DOMContentLoaded",() => {
+	const standBy = new IconButton("#stand-by");
+	const turnOff = new IconButton("#turn-off");
+	const restart = new IconButton("#restart");
+});
+
+class IconButton {
+	animClass = "icon-btn--animated";
+
+	constructor(el) {
+		this.el = document.querySelector(el);
+
+		this.init();
+	}
+	init() {
+		const events = ["focus", "mouseover", "touchstart"];
+		events.forEach(ev => {
+			this.el?.addEventListener(ev,this.iconAnimPlay.bind(this));
+		});
+
+		const animEndEl = this.el?.querySelector("[data-anim-end]");
+		animEndEl?.addEventListener("animationend",this.iconAnimStop.bind(this));
+	}
+	iconAnimPlay() {
+		this.el?.classList.add(this.animClass);
+	}
+	iconAnimStop() {
+		this.el?.classList.remove(this.animClass);
+	}
+}
